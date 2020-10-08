9cc3742f77b83f6a7dcef2a0e3f69cc98f3b38d7
JonnathanEspinoza <jonnathan.espinoza684j@gmail.com>
2020-10-07 21:40:29 -0500

    signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
-    
-    const {username, email, password, roles} = req.body;
 
+    const { username, email, password, roles } = req.body;
     //const userFound = await User.find({email});
 
     const newUser = new User({
@@ -14,13 +14,22 @@ export const signUp = async (req, res) => {
         password: await User.encryptPassword(password)
     });
 
-    const saveUser = await newUser.save();
+    if (roles) {
+        const foundRoles = await Role.find({ name: { $in: roles } });
+        newUser.roles = foundRoles.map(role => role._id);
+    } else {
+        const role = await Role.findOne({ name: "user" });
+        newUser.roles = [role._id];
+    }
 
-    const token = jwt.sign({id: saveUser._id}, config.SECRET, {
+    const saveUser = await newUser.save();
+    console.log(saveUser);
+
+    const token = jwt.sign({ id: saveUser._id }, config.SECRET, {
         expiresIn: 86400 // 24 hours
     });
 
-    res.status(200).json({token})
+    res.status(200).json({ token })
 }
 
 export const signin = async (req, res) => {
